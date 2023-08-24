2bcc8104d2f66544196a20313e2459830b0d9ab4
Laura Amelung <laam0001@stud.kea.dk>
2023-08-24 10:29:26 +0200

    DOM-element

---
+++ leg.js
@@ -0,0 +1,14 @@
+const overskrift = document.querySelector("#overskrift");
+console.log(overskrift);
+
+const info = document.querySelector("article:first-child");
+console.log(info);
+
+const p = document.querySelector("article:nth-child(2)");
+console.log(p);
+
+const boks = document.querySelector("section:last-child");
+console.log(boks);
+
+const kasse = document.querySelector("#billede1");
+console.log(kasse);
