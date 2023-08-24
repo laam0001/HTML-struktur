a8b023b035283b0d86d6c590f206fd97dd67e0ab
Laura Amelung <laam0001@stud.kea.dk>
2023-08-24 10:54:29 +0200

    billeder

--- leg.js
+++ leg.js
@@ -24,3 +24,14 @@ underoverskrift1.textContent = "Hvad skal der stå her?";
 const underoverskrift2 = document.querySelector("#underoverskrift2");
 console.log(underoverskrift2);
 underoverskrift2.textContent = "Her er en vild overskrift!";
+
+const img1 = document.querySelector("#billede1");
+console.log(img1);
+img1.src =
+  "https://fastly.picsum.photos/id/62/2000/1333.jpg?hmac=PbFIn8k0AndjiUwpOJcfHz2h-wPCQi_vJRTJZPdr6kQ";
+img1.alt = "Her er en alt teskt";
+
+const img2 = document.querySelector("#billede2");
+img2.src =
+  "https://fastly.picsum.photos/id/116/3504/2336.jpg?hmac=C46vgpj3R407e8pCyy8NhIsNaBZCjb4r5d71keNgMJY";
+img2.alt = "Her er endnu en alt teskt";
